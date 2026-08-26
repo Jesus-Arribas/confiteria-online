bd52e8c242731c605500ab23586d442fb60a7bd3
Jesús <jesus@arribas.dev>
2026-08-26 12:12:18 +0200

    feat: Create product controller. Closes #5

---
+++ confiteria-padre/confiteria-server/src/main/java/dev/arribas/confiteria/comun/Constantes.java
@@ -0,0 +1,11 @@
+package dev.arribas.confiteria.comun;
+
+public class Constantes {
+	public static final int CODIGO_ERROR_ELEMENTO_NO_ENCONTRADO = 1;
+	public static final int CODIGO_ERROR_SERVIDOR = 2;
+	public static final int CODIGO_ERROR_DAO = 3;
+	public static final int CODIGO_OK = 0;
+	public static final String MSJ_NO_EXISTE_REGISTRO = "No existe el registro";
+	public static final String MSJ_ERROR_SERVIDOR = "Error en el servidor";
+	public static final String MSJ_REGISTRO_ELIMINADO = "Se ha eliminado el registro";
+}
